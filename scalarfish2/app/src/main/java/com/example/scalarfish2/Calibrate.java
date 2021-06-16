0736a6a261fda0b75a97ca3969d42a99644c8e96
Malte <maltewollermann@yahoo.de>
2021-06-16 17:40:56 +0200

    initial

---
+++ scalarfish2/app/src/main/java/com/example/scalarfish2/Calibrate.java
@@ -0,0 +1,227 @@
+package com.example.scalarfish2;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.calib3d.*;
+
+public class Calibrate extends Fragment implements  View.OnClickListener {
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+
+    // For image capturing
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    // For accessing the elements in the fragment
+    ImageView imgView;
+    View view;
+
+    // For creating a path to save the image to
+    String currentPhotoPath;
+
+    Uri photoURI;
+    File photoFile;
+
+    Calib3d calib = new Calib3d();
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Calibrate() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Calibrate.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Calibrate newInstance(String param1, String param2) {
+        Calibrate fragment = new Calibrate();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_calibrate, container, false);
+
+        // Get the button for capturing a calibration image and set the listener
+        Button btnCaptureImg = (Button) view.findViewById(R.id.btnCaptureImg);
+        btnCaptureImg.setOnClickListener(this);
+
+        // Get the image view to display the captured image on
+        imgView = (ImageView) view.findViewById(R.id.imgViewCalibration);
+
+        return view;
+    }
+
+    // Creates an unique image file name
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        // getActivity() is required for calling the function, since this is a fragment
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",   /* suffix */
+                storageDir      /* directory */
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    // Open the camera, take a picture and save the picture
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            photoFile = null;
+            try {
+                photoFile = createImageFile();
+                Log.i("File created", "Image file created: " + photoFile.toString());
+            } catch (IOException ex) {
+                // Error while creating file
+                Log.e("File creation error", ex.toString());
+            }
+
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(getActivity(), /* 'getActivity' was 'this' in the example, but this is a fragment, not an activity */
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnCaptureImg:
+                dispatchTakePictureIntent();
+                break;
+        }
+    }
+
+    // After taking the image, set the image view to the URI of the image
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        imgView.setImageBitmap(openCvCannyFilter(photoFile));
+        //imgView.setImageBitmap(calibration((photoFile)));
+        //imgView.setImageURI(photoURI);
+        //calibration(photoFile);
+    }
+
+    // Testing openCV with a filter
+    public Bitmap openCvCannyFilter(File imgFile) {
+        Mat img = new Mat();
+
+        // Get the path of the image file and covert the file to a bitmap
+        Bitmap srcBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        Bitmap bmp32 = srcBitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        // Convert the bitmap to a Mat
+        Utils.bitmapToMat(bmp32, img);
+
+        // OpenCV stuff
+        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA);
+        Mat img_result = img.clone();
+        Imgproc.Canny(img, img_result, 80, 90);
+        Bitmap img_bitmap = Bitmap.createBitmap(img_result.cols(), img_result.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(img_result, img_bitmap);
+
+        return img_bitmap;
+    }
+
+    public void calibration(File imgFile) {
+        Mat img = new Mat();
+
+        // Get the image and convert it to a bitmap
+        Bitmap srcBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+        Bitmap bmp32 = srcBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bmp32, img);
+
+        // Create a grayscale copy of the image
+        // img_result = img.clone();
+        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2GRAY);
+
+        //img.release();
+
+        // Finding the corners of the chessboard
+        if(chessboardDetection(img)) {
+            Log.i("Chessboard info", "Corners found");
+        } else {
+            Log.i("Chessboard info", "Corners not found");
+        }
+
+        img.release();
+        System.gc();
+
+        // Convert the Mat to a bitmap and return it
+        /*Bitmap img_bitmap = Bitmap.createBitmap(img_result.cols(), img_result.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(img_result, img_bitmap);*/
+
+        //return img_bitmap;
+    }
+
+    public boolean chessboardDetection(Mat img_result) {
+        Size boardSize = new Size(9,6);
+        MatOfPoint2f imageCorners = new MatOfPoint2f();
+        boolean found = calib.findChessboardCorners(img_result, boardSize, imageCorners, Calib3d.CALIB_CB_ADAPTIVE_THRESH + Calib3d.CALIB_CB_NORMALIZE_IMAGE + Calib3d.CALIB_CB_FAST_CHECK);
+
+        imageCorners.release();
+        img_result.release();
+
+        return found;
+    }
+}
