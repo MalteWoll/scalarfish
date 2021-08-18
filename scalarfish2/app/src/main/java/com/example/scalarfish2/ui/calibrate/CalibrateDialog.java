60d71dbb9465b7aaef6f0b574abbab954cfb88b3
SolveigLotharius <solveig.lotharius@smail.th-koeln.de>
2021-08-18 15:15:39 +0200

    add_calibration_dialog

---
+++ scalarfish2/app/src/main/java/com/example/scalarfish2/ui/calibrate/CalibrateDialog.java
@@ -0,0 +1,30 @@
+package com.example.scalarfish2.ui.calibrate;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CalibrateDialog extends AppCompatDialogFragment {
+
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("How to calibrate")
+                .setMessage("tutorial")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    };
+}
